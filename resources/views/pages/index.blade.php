eb8b0c53f72a9110a6a75700f426950ad0f01c62
Jeeva7028 <nanthankjeeva@gmail.com>
2024-10-22 10:32:44 +0530

    ok

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -23,5 +23,38 @@
         </div>
     </div>
 
+    <div class="container-fluid bg-light py-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-light mb-3">
+                    <div class="card-header">What is Quiz?</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Quiz is a game</h5>
+                        <p class="card-text">Quiz is a game where you have to answer questions in a limited time. The questions are randomly generated and the answers are multiple choice. The game is designed to be fun and challenging.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-light mb-3">
+                    <div class="card-header">How to Play</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Playing is easy</h5>
+                        <p class="card-text">To play, just click on the "Start Quiz" button. You will be presented with a question and four possible answers. Choose the answer you think is correct and click on the "Submit" button. You will then be given the next question. The game continues until you have answered all the questions.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-light mb-3">
+                    <div class="card-header">Rules</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Rules are simple</h5>
+                        <p class="card-text">The game has a few simple rules. You can only answer one question at a time. You cannot go back to a previous question. You cannot skip a question. You must answer all the questions to finish the game.</p>
+                    </div>
+                </div>
+                    </div>
+                </div>
+            </div>
+       
+
 @endsection
  
